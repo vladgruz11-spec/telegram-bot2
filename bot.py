096b2cd40558b3605dc0b2e12c6f000d66535304
vladgruz11-spec <vladgruz11@gmail.com>
2026-05-15 17:19:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -218,7 +218,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "Шаг 1: Перед тем как начать, подпишись на канал https://t.me/+dFJBLVKcU_BkNTY6, чтобы нас не потерять, если бота заблокируют!\n\n"
         "Затем возвращайся, и приступим к СОЗДАНИЮ ВИДЕО\n\n"
-        "Шаг 1: отправь мне картинку."
+        "Шаг 2: отправь мне картинку."
     )
 
 
