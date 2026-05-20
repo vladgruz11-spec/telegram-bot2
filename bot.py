235a39947eafbee1af6923da5816a5b71adc33ed
vladgruz11-spec <vladgruz11@gmail.com>
2026-05-20 15:38:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -532,22 +532,16 @@ async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
     duration = user_states[user_id]["duration"]
     video_cost = VIDEO_PRICES[duration]
 
-    free_used_after, paid_credits_after = get_user(user_id)
-    
-    await update.message.reply_text(
-            f"Баланс: {paid_credits_after} ₽"
-        )
-
-    await update.message.reply_text(
-            f"Баланс: {paid_credits_after} ₽"
-        )
-
-    if paid_credits_after <= 0:
+    if free_used >= 1 and paid_credits < video_cost:
         await update.message.reply_text(
-                "💳 Бесплатные генерации закончились.\n\n"
-                "👇Купить или получить БЕСПЛАТНО👇",
-                reply_markup=paid_menu()
-            )
+            f"Баланс: {paid_credits} ₽"
+        )
+
+        await update.message.reply_text(
+            "💳 Бесплатные генерации закончились.\n\n"
+            "👇Купить или получить БЕСПЛАТНО👇",
+            reply_markup=paid_menu()
+        )
         return
 
     await update.message.reply_text(
