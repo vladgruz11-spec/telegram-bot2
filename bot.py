6a1e978fe3aeff6c9147f6e628fc96caad7f14cd
vladgruz11-spec <vladgruz11@gmail.com>
2026-06-04 13:37:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -143,7 +143,7 @@ VIDEO_PRICES = {
 }
 
 
-EXAMPLES_CHANNEL_URL = "https://t.me/+CScI2LQknNxlY2Rj"
+EXAMPLES_CHANNEL_URL = "https://t.me/+ClqpWYaWiIYzZWI6"
 MAIN_CHANNEL_URL = "https://t.me/Xena18H"
 
 def main_inline_menu():
